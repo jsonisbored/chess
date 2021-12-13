0262d96080d92ae6f17632d0e7c8b68fe301ad64
JSON <32702660+jsonisbored@users.noreply.github.com>
2021-12-12 18:40:34 -0600

    Initial commit

---
+++ js/util.js
@@ -0,0 +1,46 @@
+export function toDests(chess) {
+    const dests = new Map();
+    chess.SQUARES.forEach(s => {
+        const ms = chess.moves({square: s, verbose: true});
+        if (ms.length) dests.set(s, ms.map(m => m.to));
+    });
+    return dests;
+}
+
+export function toColor(chess) {
+    return (chess.turn() === 'w') ? 'white' : 'black';
+
+    }
+
+export function playOtherSide(cg, chess) {
+    return (orig, dest) => {
+        chess.move({from: orig, to: dest});
+        cg.set({
+        turnColor: toColor(chess),
+        movable: {
+            color: toColor(chess),
+            dests: toDests(chess)
+        }
+        });
+    };
+}
+
+export function aiPlay(cg, chess, delay, firstMove) {
+    return (orig, dest) => {
+        chess.move({from: orig, to: dest});
+        setTimeout(() => {
+        const moves = chess.moves({verbose:true});
+        const move = firstMove ? moves[0] : moves[Math.floor(Math.random() * moves.length)];
+        chess.move(move.san);
+        cg.move(move.from, move.to);
+        cg.set({
+            turnColor: toColor(chess),
+            movable: {
+            color: toColor(chess),
+            dests: toDests(chess)
+            }
+        });
+        cg.playPremove();
+        }, delay);
+    };
+}
